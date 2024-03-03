39d9ba51e5357762b4e4e2374dc22822d0b5259a
Gary Coy <32619147+garycoy@users.noreply.github.com>
2024-03-03 11:50:58 -0600

    Create virtual_screen_setup.sh

---
+++ virtual_screen_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+EXT_MONITOR=$1
+LAPTOP=eDP-1
+
+if [[ "XX${EXT_MONITOR}XX" == "XXXX" ]];then
+   echo "You did not put in connection for external monitor."
+fi
+
+xrandr --output ${EXT_MONITOR} --primary
+
+xrandr --listactivemonitors
+
+xrandr --output ${LAPTOP} --off
+
+xrandr --setmonitor left 1918/535x1080/300+0+0 ${EXT_MONITOR}
+xrandr --setmonitor right 1918/535x1080/300+1919+0 none
+xrandr --fb 3840x1081
+xrandr --fb 3840x1080
+
+xrandr --output ${LAPTOP} --mode 1920x1080
